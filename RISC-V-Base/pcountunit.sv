efb1492c36994b053e34431afb2db6a97e2fcb42
SsagatroNN <omar.khatib03@outlook.com>
2022-12-06 22:59:35 +0000

    Added jump multiplexers and set the PC counter to the strating point of the memory Co-authored-by: Yi Zhang Co-authored-by: Yazan Ayoub Co-authored-by: Jie Chen

--- RISC-V-Base/pcountunit.sv
+++ RISC-V-Base/pcountunit.sv
@@ -5,20 +5,25 @@ module pcountunit #(
     input logic pcsrc,
     input logic clk,
     input logic rst,
-    input logic [WIDTH-1:0] immop,
+    input logic jbmux,
+    input logic [WIDTH-1:0] jumpaddress,
     output logic [WIDTH-1:0] pc
 );
 
   logic [WIDTH-1:0] next_pc;
   logic [WIDTH-1:0] branch_pc;
 
+  initial begin
+    assign branch_pc = 32'hBFC00000;
+  end
+
   always_comb begin
-    branch_pc = pc + immop;
+    branch_pc = jbmux ? jumpaddress : pc + jumpaddress;
     next_pc   = pcsrc ? branch_pc : pc + 4;
   end
 
   always_ff @(posedge clk) begin
-    if (rst) pc <= 32'b0;
+    if (rst) pc <= 32'hBFC00000;
     else pc <= next_pc;
   end
 
