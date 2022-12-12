7ba581c5911c501cff88eaf138a18b861f359f99
3136321049 <3136321049@qq.com>
2022-12-12 12:43:49 +0000

    remove jump mux

--- RISC-V-Single-Cycle/RTL/fetch.sv
+++ RISC-V-Single-Cycle/RTL/fetch.sv
@@ -1,10 +1,9 @@
 module fetch #(
   parameter A_WIDTH = 32,
-            D_WIDTH = 8
 )(
-    input logic pcsrc, clk, rst, jbmux,
+    input logic pcsrc, clk, rst, 
     input logic [A_WIDTH-1:0] jumpaddress,      //
-    output [31:0] dout,                  //instruction mem out
+    output [A_WIDTH-1:0] dout,                  //instruction mem out
     output [A_WIDTH-1:0] pc,
     output [A_WIDTH-1:0] pc_plus4
 );
@@ -13,7 +12,6 @@ pcountunit pcountunit(
   .pcsrc(pcsrc),
   .clk(clk),
   .rst(rst),
-  .jbmux(jbmux),
   .jumpaddress(jumpaddress),
   .pc(pc),
   .pc_plus4(pc_plus4)
--- RISC-V-Single-Cycle/RTL/pcountunit.sv
+++ RISC-V-Single-Cycle/RTL/pcountunit.sv
@@ -2,22 +2,16 @@
 module pcountunit #(
     parameter WIDTH = 32
 ) (
-    input logic pcsrc, clk, rst, jbmux,
+    input logic pcsrc, clk, rst,
     input logic [WIDTH-1:0] jumpaddress,
     output logic [WIDTH-1:0] pc,
     output logic [WIDTH-1:0] pc_plus4  // newly added output for register
 );
 
   logic [WIDTH-1:0] next_pc;
-  logic [WIDTH-1:0] branch_pc;
-
-  initial begin
-    assign branch_pc = 32'hBFC00000;
-  end
 
   always_comb begin
-    branch_pc = jbmux ? jumpaddress : pc + jumpaddress;
-    next_pc   = pcsrc ? branch_pc : pc + 4;
+    next_pc   = pcsrc ? jumpaddress : pc + 4;
   end
 
   always_ff @(posedge clk) begin
