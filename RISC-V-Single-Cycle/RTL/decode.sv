25fbb39fc9214495aa1ab194f116633d3b08e786
yi <yz7420@ic.ac.uk>
2022-12-12 17:14:42 +0000

    fixed wire width and input/output error for decode.sv

--- RISC-V-Single-Cycle/RTL/decode.sv
+++ RISC-V-Single-Cycle/RTL/decode.sv
@@ -1,20 +1,20 @@
 module decode(
-    input logic [31:0] wd3D, pcF, pcplusfourF, a0D, instrD,
+    input logic [31:0] wd3D, pcF, pcplusfourF, instrD,
     input logic we3D, clk, triggerD,
     input logic [4:0] ad3D,
     output logic regwriteD, resultsrcD, memwriteD, alusrcD, addupperD, jbmuxD, pcwritemuxD,
     output logic [3:0] aluctrlD,
-    output logic [31:0] pcD, pcplusfourD, immopD, rd1D, rd2D,
+    output logic [31:0] pcD, pcplusfourD, immopD, rd1D, rd2D, a0D,
     output logic [4:0] rdD
 );
 
-    wire [2:0] immsrcD;
-    wire funct3D = instrD[14:12];
     wire funct7D = instrD[30];
-    wire opcodeD = instrD[6:0];
-    wire signextinD = instrD[31:7];
-    wire ad1D = instrD[19:15];
-    wire ad2D = instrD[24:20];
+    wire [2:0] immsrcD;
+    wire [2:0] funct3D = instrD[14:12];
+    wire [6:0]opcodeD = instrD[6:0];
+    wire [24:0]signextinD = instrD[31:7];
+    wire [4:0]ad1D = instrD[19:15];
+    wire [4:0]ad2D = instrD[24:20];
 
     assign rdD = instrD[11:7];
     assign pcD = pcF;
