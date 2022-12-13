b9da5ea4629099753d8cbe897df4cf73ca6818a2
SsagatroNN <omar.khatib03@outlook.com>
2022-12-13 21:01:29 +0000

    Fixed bug and added counter.s file

---
+++ RISC-V-Single-Cycle/programs/counter.s
@@ -0,0 +1,17 @@
+main:
+    addi t1, zero, 0xff; # t1 = zero + 0xff
+    addi a0, zero, 0x0; # a0 = zero + 0x0
+
+mloop:
+    addi a1, zero, 0xf0; # a1 = zero + 0xf0
+    nop
+    nop
+    
+iloop:
+    addi a0, a1, 0; # a0 = a1 + 0
+    addi a1, a1, 1; # a1 = a1 + 1
+    nop
+    nop
+    bne a1, t1, iloop # if a1 != t1 then iloop
+    bne t1, zero, mloop # if t1 != zero then mloop
+    
