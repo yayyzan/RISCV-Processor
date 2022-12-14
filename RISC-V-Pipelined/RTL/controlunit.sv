afc2fba670b6e77c91ebf07d2b91687c3cdedacb
Yazan Ayyoub <yazanayyoub44@Yazan-Ayyoubs-Macbook.local>
2022-12-14 18:08:05 +0300

    made changes to and simplified control unit in accordance with original pipelined cpu, tested on all 5 tests

--- RISC-V-Pipelined/RTL/controlunit.sv
+++ RISC-V-Pipelined/RTL/controlunit.sv
@@ -1,41 +1,51 @@
 module controlunit (
-    input logic [6:0] opcode,
+    input logic [6:0] opcode, 
     input logic [2:0] funct3,
-    input logic funct7, 
-    output logic regwrite, alusrc, memwrite, resultsrc, pcwritemux, jbmux, addupper,
+    input logic funct7,
+    output logic regwrite, alusrc, memwrite, resultsrc, pcwritemux, jbmux, addupper, 
     output logic [3:0] aluctrl,
     output logic [2:0] immsrc
 );
 
-  wire [1:0] aluop = {(opcode == 7'h33 | opcode == 7'h13 | opcode == 7'h37), (opcode == 7'h37 | opcode == 7'h63)};
+  logic rg = opcode == 51; 
+  logic imm = opcode == 19; 
+  logic load = opcode == 3;
+  logic store = opcode == 35;
+  logic branch = opcode == 99;
+  logic ldupper = opcode == 55;
+  logic aupper = opcode == 23;
+  logic jalr = opcode == 103; 
+  logic jal = opcode == 111;
+  
+  wire [1:0] aluop = {(rg | imm | ldupper), (ldupper | branch)};
 
-  always_comb begin
-    regwrite = opcode == 7'h13 | opcode == 7'h03 | opcode == 7'h33 | opcode >= 7'h67 | opcode == 7'h17 | opcode == 7'h37; 
-    alusrc = opcode == 7'h03 | opcode == 7'h13 | opcode == 7'h23 | opcode == 7'h67 | opcode == 7'h17 | opcode == 7'h37; 
-    memwrite = opcode == 7'h23;  
-    resultsrc = opcode == 7'h03; 
-    jbmux = opcode == 7'h67; // selects which address pc should jump to
-    pcwritemux = opcode >= 7'h67; // chooses between writing register with PC+4 or result from alu or load instruction
-    addupper = opcode == 7'h17;
+  always_comb begin // main decoder
+    regwrite = !store & !branch; 
+    alusrc = !rg & !branch & !jal;
+    memwrite = store; 
+    resultsrc = load; 
+    jbmux = jalr;  // selects which address pc should jump to
+    pcwritemux = jalr | jal; // chooses between writing register with PC+4 or result from alu or load instruction
+    addupper = aupper;
+
     case (opcode) 
-      7'h13:   immsrc = 3'h0;  // imm
-      7'h03:   immsrc = 3'h0;  // load
-      7'h23:   immsrc = 3'h2;  // store
-      7'h63:   immsrc = 3'h3;  // branch
-      7'h33:   immsrc = 3'h7;  // reg
-      7'h37:   immsrc = 3'h1;  // upp imm (load)
-      7'h17:   immsrc = 3'h1;  // upp imm (add)
-      default: immsrc = 3'h4;  // jump
+      19:   immsrc = 0;  // imm
+      3:    immsrc = 0;  // load
+      35:   immsrc = 2;  // store
+      99:   immsrc = 3;  // branch
+      55:   immsrc = 1;  // upp imm (load)
+      23:   immsrc = 1;  // upp imm (add)
+      default:  immsrc = 4;  // jump
     endcase
   end
 
   always_comb begin // aluctrl
     case (aluop)
-      2'h0: aluctrl = 4'h0; // load and store and add upper
-      2'h1: aluctrl = {1'h0, !funct3[2], funct3[2], funct3[1]}; // branch
-      2'h2: aluctrl = {funct7 & ((opcode == 7'h13 & funct3 == 3'h5) | (opcode == 7'h33 & (funct3 == 3'h5 | funct3 == 3'h0))), funct3}; // reg or imm
-      2'h3: aluctrl = 4'hf; //load upper
-      default: aluctrl = 4'h9;
+      0: aluctrl = 0; // load or store or add upper
+      1: aluctrl = {1'b0, !funct3[2], funct3[2], funct3[1]}; // branch
+      2: aluctrl = {funct7 & ((imm & funct3 == 5) | (rg & (funct3 == 5| funct3 == 0))), funct3}; // reg or imm
+      3: aluctrl = 15; //load upper
+      default: aluctrl = 9;
     endcase
   end
     
